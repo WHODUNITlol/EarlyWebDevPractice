ebbd808efc266ae211ce174f50100645140b47a0
Davey Pushard <daveypushard@gmail.com>
2018-04-26 23:45:34 -0400

    finished quiz functionality and edited style of quiz page

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,18 @@
-let button = document.getElementByID("option");
+let answer = document.querySelectorAll(".button");
+let response = document.querySelector("#response")
+
+answer.forEach(function(param){
+    param.addEventListener('click', function(){
+        if(param.id === "option") {
+            response.childNodes[0].src="../images/thumbsup.png";
+            response.style.display = "block";
+        }
+        else {
+            response.childNodes[0].src="../images/Poopemoji.png";
+            response.style.display = "block";
+        }
+    });
+});
+
+
+
